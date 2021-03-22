941ead6e41914a855aa3f8ce82ca1bf5bf2a2ce4
Kat Hurdley <katherine.hurdley@googlemail.com>
2021-03-22 17:56:52 +0000

    22.3.21

--- DCP.js
+++ DCP.js
@@ -41,5 +41,68 @@ function multiplier(array){
     } return newArray;
 }
 
-console.log(multiplier([3, 2, 1]));
-console.log(multiplier([1, 2, 3, 4, 5]));
+// console.log(multiplier([3, 2, 1]));
+// console.log(multiplier([1, 2, 3, 4, 5]));
+
+
+
+function dividesby3(num){
+// Split number into separate integers (as an array?)
+ let digits = [];
+ let solution = num.charAt(0)
+ // Loop through array and add numbers together
+ for (let i = 1; i<digits.length; i++){
+    digits.push(num.charAt(i));
+ }
+ for (let digit of digits){
+     solution += digit
+ }
+ // Test whether the result divides by three
+ return (solution%3===0)
+}
+
+console.log(dividesby3('99'))
+console.log(dividesby3('85'))
+
+
+function divideBy3(num){
+    // Create new variable and take first digit
+     let solution = parseInt(num.charAt(0))
+     // Loop through string and add numbers together
+     for (let i = 1; i<num.length; i++){
+        solution += parseInt(num.charAt(i));
+     }
+     // Test whether the result divides by three
+     return (solution%3===0)
+    }
+
+    console.log(divideBy3('99'))
+    console.log(divideBy3('85'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Split number into separate integers (as an array?)
+// Loop through array and add numbers together
+// Test whether the result divides by three
+// Return true or false
+
+
+
